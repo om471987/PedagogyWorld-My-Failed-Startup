261bc57b7016a5d3ae0546f57aa357fb4b52c68e
omkarpanhalkar <omkar.panhalkar@live.com>
2013-02-28 06:54:06 -0800

    working copy

---
+++ PedagogyWorld.Services/HtmlToPdf.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace PedagogyWorld.Services
+{
+    public class HtmlToPdf
+    {
+        public bool Convert(string Url, string filename)
+        {
+            // assemble destination PDF file name
+            //string filename = ConfigurationManager.AppSettings["ExportFilePath"] + "\\" + output + ".pdf";
+
+            // get proj no for header
+            //Project project = new Project(int.Parse(output));
+
+            var p = new Process();
+            p.StartInfo.FileName = @"C:\Program Files (x86)\wkhtmltopdf\wkhtmltopdf.exe";//ConfigurationManager.AppSettings["HtmlToPdfExePath"];
+
+            string switches = "--print-media-type ";
+            switches += "--margin-top 4mm --margin-bottom 4mm --margin-right 0mm --margin-left 0mm ";
+            switches += "--page-size A4 ";
+            switches += "--no-background ";
+            switches += "--redirect-delay 100";
+
+            p.StartInfo.Arguments = switches + " " + Url + " " + filename;
+
+            p.StartInfo.UseShellExecute = false; // needs to be false in order to redirect output
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardInput = true; // redirect all 3, as it should be all 3 or none
+            //p.StartInfo.WorkingDirectory = @"C:\Users\omkar\Desktop";//StripFilenameFromFullPath(p.StartInfo.FileName);
+            p.Start();
+
+            // read the output here...
+            string output = p.StandardOutput.ReadToEnd();
+
+            // read the exit code, close process
+            int returnCode = p.ExitCode;
+            p.Close();
+
+            // if 0 or 2, it worked (not sure about other values, I want a better way to confirm this)
+            return (returnCode == 0 || returnCode == 2);
+        }
+    }
+}
--- PedagogyWorld/Controllers/HomeController.cs
+++ PedagogyWorld/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using PedagogyWorld.Services;
+using System.Text;
+using System.Web.Mvc;
+using System.Threading;
 
 namespace PedagogyWorld.Controllers
 {
@@ -12,8 +16,19 @@ namespace PedagogyWorld.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
-
-            return View();
+            var url = "http://localhost:20000/File/Planner";
+            var filename = @"C:\Users\omkar\Desktop\t1.pdf";
+            var a = new HtmlToPdf();
+            if (a.Convert(url, filename))
+            {
+                Thread.Sleep(6000);
+                var file = System.IO.File.ReadAllText(filename);
+                return File(Encoding.UTF8.GetBytes(file), "application/pdf", "t2.pdf");
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult Contact()
